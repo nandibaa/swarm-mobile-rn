a3e6bf5060364b8c9e553fc24ffadff0d9e81fca
Nandor Komlodi <nandor.komlodi@gmail.com>
2026-01-28 10:29:58 +0100

    feature: refactor use mmkv instead of expo secure store

--- app/store/app.store.ts
+++ app/store/app.store.ts
@@ -1,6 +1,17 @@
 import { NodeStatus } from '@/constants/types';
 import { create } from 'zustand/react';
 
+import { createMMKV } from 'react-native-mmkv';
+
+export const storage = createMMKV();
+
+const defaultRpcEndpoint = 'https://xdai.fairdatasociety.org';
+
+export const STORAGE_KEYS = {
+  PASSWORD: 'swarm_node_password',
+  RPC_ENDPOINT: 'swarm_node_rpc_endpoint',
+};
+
 export interface AppData {
   swarmHash: string;
   nodeStatus: NodeStatus;
@@ -8,6 +19,11 @@ export interface AppData {
   walletAddress: string;
   connectedPeers: number;
   loading: boolean;
+
+  getPassword: () => string;
+  setPassword: (password: string) => void;
+  getRpcEndpoint: () => string;
+  setRpcEndpoint: (endpoint: string) => void;
 
   updateHash: (name: string) => void;
   updatePeersCount: (connectedPeers: number) => void;
@@ -25,6 +41,14 @@ export const useAppStore = create<AppData>((set) => ({
   walletAddress: '',
   connectedPeers: 0,
   loading: false,
+
+  getPassword: () => storage.getString(STORAGE_KEYS.PASSWORD) || '',
+  setPassword: (password: string) =>
+    storage.set(STORAGE_KEYS.PASSWORD, password),
+  getRpcEndpoint: () =>
+    storage.getString(STORAGE_KEYS.RPC_ENDPOINT) || defaultRpcEndpoint,
+  setRpcEndpoint: (endpoint: string) =>
+    storage.set(STORAGE_KEYS.RPC_ENDPOINT, endpoint),
 
   updateHash: (hash: string) => set({ swarmHash: hash }),
   updatePeersCount: (connectedPeers: number) => set({ connectedPeers }),
